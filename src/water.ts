a3d9a50406b085e9adf5d387f42f11582a57d622
tongtong.ctt <tongtong.ctt@alibaba-inc.com>
2020-08-12 21:16:29 +0800

    feat: request dignding

--- src/water.ts
+++ src/water.ts
@@ -1,5 +1,8 @@
 import { Func, Inject, Provide } from '@midwayjs/decorator';
 import { FaaSContext, FunctionHandler } from '@midwayjs/faas';
+import { robotUri } from '../private/config';
+
+const request = require('request-promise');
 
 @Provide()
 @Func('water.handler')
@@ -8,6 +11,27 @@ export class WaterService implements FunctionHandler {
   ctx: FaaSContext; // context
 
   async handler() {
-    return 'hello world';
+    const query = this.ctx.req.pathParameters || {};
+    const data = {
+      msgtype: 'text',
+      text: {
+        content: `呐呐~ ${query.message || ''}`
+      },
+      at: {
+        atMobiles: [],
+        isAtAll: false
+      }
+    };
+
+    const result = await request(robotUri, {
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      json: true
+    });
+
+    return result;
   }
 }
